3e5fe3a4c1c1ce55cc93a14927662209d3bc1dd2
Leo E <leoem@b12dev.com>
2021-01-10 15:55:58 -0300

    Pycharm Project Files Se agrega al producto el campo costo_postproceso

--- abastece/admin.py
+++ abastece/admin.py
@@ -1,24 +1,29 @@
 from django.contrib import admin
 
-from .models import Producto, ImagenProducto, Contacto, ImagenContacto, Nodo, Membresia, ProductoVariedad, Ciclo, Domicilio
+from .models import Producto, ImagenProducto, Contacto, ImagenContacto, Nodo, Membresia, ProductoVariedad, Ciclo, \
+    Domicilio
 
 
 ##  CONTACTO
 class ProductoInline(admin.TabularInline):
     model = Producto
     extra = 3
-    
+
+
 class ImagenContactoInline(admin.StackedInline):
     model = ImagenContacto
     extra = 1
-    
+
+
 @admin.register(Contacto)
 class ContactoAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['nombre', 'apellido', 'nombre_fantasia', 'descripcion', 'web']}),
+        (None, {'fields': ['nombre', 'apellido', 'nombre_fantasia', 'descripcion', 'web']}),
         ('Datos de Contacto', {'fields': ['telefono', 'domicilio', 'email'], 'classes': ['collapse']}),
     ]
     inlines = [ProductoInline, ImagenContactoInline]
+
+
 ##
 
 ## PRODUCTO
@@ -26,31 +31,39 @@ class ContactoAdmin(admin.ModelAdmin):
 class ProductoVariedadInline(admin.TabularInline):
     model = ProductoVariedad
     extra = 1
-    
+
+
 class ImagenProductoInline(admin.StackedInline):
     model = ImagenProducto
     extra = 1
+
+
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['productor', 'titulo', 'descripcion']}),
         ('Presentación', {'fields': [('envase', 'cantidad', 'unidad')]}),
-        ('Costos', {'fields': [('costo_produccion', 'costo_financiero', 'costo_transporte')]})
+        ('Costos', {'fields': [('costo_produccion', 'costo_financiero', 'costo_transporte', 'costo_postproceso')]})
     ]
     inlines = [ProductoVariedadInline, ImagenProductoInline]
+
+
 ##
 
 ## NODO
 class MembresiaInline(admin.StackedInline):
     model = Membresia
     extra = 1
-    
+
+
 @admin.register(Nodo)
 class NodoAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['nombre', 'domicilio', 'domicilio_recepcion']}),
     ]
     inlines = [MembresiaInline]
+
+
 ##
 
 ## DOMICILIO
--- abastece/models.py
+++ abastece/models.py
@@ -1,50 +1,56 @@
 from django.db import models
 
+
 class Domicilio(models.Model):
-    
     pais = models.CharField(max_length=200, default="Argentina")
     provincia = models.CharField(max_length=100, default="Catamarca")
     localidad = models.CharField(max_length=100)
     direccion = models.CharField(max_length=200)
     codigo_postal = models.CharField(max_length=100)
-    
+
     def __str__(self):
-        return "{}, {} CP {}, {}, {}".format(self.direccion, self.localidad, self.codigo_postal, self.provincia, self.pais)
+        return "{}, {} CP {}, {}, {}".format(self.direccion, self.localidad, self.codigo_postal, self.provincia,
+                                             self.pais)
+
 
 class Contacto(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
-    nombre_fantasia = models.CharField(max_length=100, blank = True)
+    nombre_fantasia = models.CharField(max_length=100, blank=True)
     telefono = models.CharField(max_length=100, blank=True)
     domicilio = models.ForeignKey(Domicilio, models.SET_NULL, null=True, blank=True)
     email = models.EmailField(blank=True)
     descripcion = models.TextField(blank=True)
     web = models.URLField(blank=True)
-    
+
     def __str__(self):
-        return self.nombre_fantasia or "{} {}".format(self.nombre,self.apellido)
-    
+        return self.nombre_fantasia or "{} {}".format(self.nombre, self.apellido)
+
+
 class ImagenContacto(models.Model):
-    contacto=models.ForeignKey(Contacto, models.CASCADE)
-    imagen=models.ImageField
-    
+    contacto = models.ForeignKey(Contacto, models.CASCADE)
+    imagen = models.ImageField
+
+
 class Nodo(models.Model):
     nombre = models.CharField(max_length=200)
-    domicilio = models.ForeignKey(Domicilio, models.PROTECT) #Domicilio de funcionamiento del nodo
+    domicilio = models.ForeignKey(Domicilio, models.PROTECT)  # Domicilio de funcionamiento del nodo
     domicilio_recepcion = models.ForeignKey(Domicilio,
-                                            models.SET_NULL, 
-                                            null = True, 
-                                            blank = True, 
+                                            models.SET_NULL,
+                                            null=True,
+                                            blank=True,
                                             related_name="domicilio_recepcion",
-                                            help_text="Solo cuando la mercadería se recibe en un domicilio diferente del de funcionamiento.")
+                                            help_text="Solo cuando la mercadería se recibe en un domicilio diferente "
+                                                      "del de funcionamiento.")
 
     def __str__(self):
         return self.nombre
-    
+
+
 class Membresia(models.Model):
     ROLES_NODO = (
-      (1, 'comun'),
-      (2, 'referente'),
+        (1, 'comun'),
+        (2, 'referente'),
     )
 
     rol = models.PositiveSmallIntegerField(choices=ROLES_NODO)
@@ -54,53 +60,60 @@ class Membresia(models.Model):
 
 class Producto(models.Model):
     UNIDADES = (
-      ("U", 'unidades'),
-      ("g", 'gramos'),
-      ("kg", 'kilogramos'),
-      ("cm3", 'centímetros cubicos'),
-      ("ml", 'mililitros'),
-      ("l", 'litros'),
-      ("m", 'metros')
+        ("U", 'unidades'),
+        ("g", 'gramos'),
+        ("kg", 'kilogramos'),
+        ("cm3", 'centímetros cubicos'),
+        ("ml", 'mililitros'),
+        ("l", 'litros'),
+        ("m", 'metros')
     )
     productor = models.ForeignKey(Contacto, models.CASCADE)
-    titulo = models.CharField(max_length=200, unique = True)
+    titulo = models.CharField(max_length=200, unique=True)
     descripcion = models.TextField(blank=True)
     envase = models.CharField(max_length=200, blank=True)
-    cantidad = models.IntegerField() #Ver de agregar MinValueValidator
-    unidad = models.CharField(max_length=100, choices=UNIDADES) ## VER de poner choice
+    cantidad = models.IntegerField()  # Ver de agregar MinValueValidator
+    unidad = models.CharField(max_length=100, choices=UNIDADES)  # VER de poner choice
     costo_produccion = models.DecimalField(max_digits=8, decimal_places=2)
     costo_transporte = models.DecimalField(max_digits=8, decimal_places=2)
     costo_financiero = models.DecimalField(max_digits=8, decimal_places=2)
-    
+    costo_postproceso = models.DecimalField(max_digits=8, decimal_places=2)
+
     def __str__(self):
         return self.titulo
-    
+
+
 class ProductoVariedad(models.Model):
     producto = models.ForeignKey(Producto, models.CASCADE)
     descripcion = models.CharField(max_length=200)
-    disponible = models.IntegerField() #Ver de agregar MinValueValidator    
+    disponible = models.IntegerField()  # Ver de agregar MinValueValidator
 
     def __str__(self):
         return self.descripcion
-    
+
+
 class ImagenProducto(models.Model):
-    producto=models.ForeignKey(Producto, models.CASCADE)
-    imagen=models.ImageField
+    producto = models.ForeignKey(Producto, models.CASCADE)
+    imagen = models.ImageField
     # https://stackoverflow.com/a/537966
-    
+
+
 class Ciclo(models.Model):
-    inicio = models.DateTimeField('inicio pedidos')    
+    inicio = models.DateTimeField('inicio pedidos')
     cierre = models.DateTimeField('cierre pedidos')
-    
-class ProductoCiclo(models.Model):   ###O va ProductoVariedad?
+
+
+class ProductoCiclo(models.Model):  ###O va ProductoVariedad?
     ciclo = models.ForeignKey(Ciclo, models.CASCADE)
     producto = models.ForeignKey(Producto, models.CASCADE)
     precio = models.DecimalField(max_digits=8, decimal_places=2)
 
+
 class Pedido(models.Model):
     ciclo = models.ForeignKey(Ciclo, models.CASCADE)
-    timestamp = models.DateTimeField('fecha y hora') #Hacer Editable = False
+    timestamp = models.DateTimeField('fecha y hora')  # Hacer Editable = False
     consumidor = models.ForeignKey(Membresia, models.CASCADE)
+
 
 class ItemPedido(models.Model):
     pedido = models.ForeignKey(Pedido, models.CASCADE)
