98a61f9f2acca14ed29558faa7ed8681fbb55068
Leo E <leoem@b12dev.com>
2021-10-29 21:27:53 -0300

    Restablecer contraseña

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,6 +4,17 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='usuarios/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout', ),
-    path('password_reset/', auth_views.LoginView.as_view(template_name='usuarios/password_reset.html'),
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(template_name='usuarios/password_reset.html',
+                                              email_template_name='usuarios/password_reset_email.html'),
          name='password_reset', ),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='usuarios/password_reset_done.html'),
+         name='password_reset_done', ),
+    path('password_reset/confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='usuarios/password_reset_confirm.html'),
+         name='password_reset_confirm', ),
+    path('password_reset/complete/',
+         auth_views.PasswordResetDoneView.as_view(template_name='usuarios/password_reset_complete.html'),
+         name='password_reset_complete', ),
 ]
