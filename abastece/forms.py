797d2d99145defb6c423162c1370a3a75184648b
Leo E <leoem@b12dev.com>
2022-06-03 23:53:11 -0300

    Fix aporte_nodo en resumen, que ahora es una property.

--- abastece/forms.py
+++ abastece/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.forms import inlineformset_factory
+from django.forms import inlineformset_factory, Select, NumberInput
 
 from .models import ItemPedido, Pedido, ProductoVariedadCiclo
 
@@ -13,5 +13,13 @@ class ItemPedidoForm(forms.ModelForm):
     class Meta:
         model = ItemPedido
         exclude = ()
+        widgets = {
+            'producto_variedad_ciclo': Select(attrs={
+                'class': "pedido-item_producto-variedad form-control",
+            }),
+            'cantidad': NumberInput(attrs={
+                'class': "pedido-item_cantidad form-control",
+            }),
+        }
 
 ItemPedidoFormset = inlineformset_factory(Pedido, ItemPedido, form=ItemPedidoForm, extra=1, can_delete=True)
